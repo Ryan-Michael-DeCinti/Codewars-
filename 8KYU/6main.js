3bffb9b070ec5eb5a333bedf1fba2df4d597d676
Ryan Michael DeCinti <101354942+Ryan-Michael-DeCinti@users.noreply.github.com>
2022-04-14 12:57:11 -0400

    Create 6main.js

---
+++ 8KYU/6main.js
@@ -0,0 +1,8 @@
+function stringToNumber(string){
+  var convert = Number(string);
+  return (convert);
+}
+stringToNumber("1234")
+stringToNumber("605")
+stringToNumber("1405")
+stringToNumber("-7")
